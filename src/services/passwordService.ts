d508c3ab5d7724c2da75160cee8d90fbb88995db
Guilherme Bley <guilhermecbley@gmail.com>
2024-06-18 22:54:30 -0300

    Creating PasswordConfig

--- src/services/passwordService.ts
+++ src/services/passwordService.ts
@@ -1,6 +1,37 @@
-export default function generatePass(){
+class PasswordConfig {
+  allowLetters: boolean;
+  allowNumbers: boolean;
+  allowSymbols: boolean;
+
+  constructor(
+    allowLetters: boolean = true,
+    allowNumbers: boolean = true,
+    allowSymbols: boolean = true) {
+      this.allowLetters = allowLetters;
+      this.allowNumbers = allowNumbers;
+      this.allowSymbols = allowSymbols;
+  }
+
+  getCharacters(): string {
+    let allowedChars = '';
+
+    if (allowedChars)
+      allowedChars += 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (this.allowSymbols)
+      allowedChars += '+_)!@#%¨$%¨¨&*(^`:Ç^>|\\/?*';
+    if (this.allowLetters)
+      allowedChars += '1234567890';
+
+    return allowedChars;
+  }
+}
+
+export default function generatePass(passwordConfig: PasswordConfig | null = null) {
+  if (passwordConfig == null)
+    passwordConfig = new PasswordConfig()
+
   let password: string = ''
-  let characters:string = 'Aa@$#123456789abcdefghijFfGHIJKkLMNOPpZz!'
+  let characters:string = passwordConfig.getCharacters()
   let passwordLength = 8
 
   for(let index = 0; index < passwordLength; index++){
