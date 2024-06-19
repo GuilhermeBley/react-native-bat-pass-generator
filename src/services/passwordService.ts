85cc5e159adc757354a66b37409cc576ea223528
Guilherme Bley <guilhermecbley@gmail.com>
2024-06-18 22:58:20 -0300

    adding length config

--- src/services/passwordService.ts
+++ src/services/passwordService.ts
@@ -2,14 +2,17 @@ class PasswordConfig {
   allowLetters: boolean;
   allowNumbers: boolean;
   allowSymbols: boolean;
+  length: number;
 
   constructor(
     allowLetters: boolean = true,
     allowNumbers: boolean = true,
-    allowSymbols: boolean = true) {
-      this.allowLetters = allowLetters;
-      this.allowNumbers = allowNumbers;
-      this.allowSymbols = allowSymbols;
+    allowSymbols: boolean = true,
+    length: number = 8) {
+    this.allowLetters = allowLetters;
+    this.allowNumbers = allowNumbers;
+    this.allowSymbols = allowSymbols;
+    this.length = length;
   }
 
   getCharacters(): string {
@@ -26,13 +29,13 @@ class PasswordConfig {
   }
 }
 
-export default function generatePass(passwordConfig: PasswordConfig | null = null) {
+export default function generatePass(passwordConfig: PasswordConfig | null = null) : string {
   if (passwordConfig == null)
     passwordConfig = new PasswordConfig()
 
   let password: string = ''
   let characters:string = passwordConfig.getCharacters()
-  let passwordLength = 8
+  let passwordLength = passwordConfig.length
 
   for(let index = 0; index < passwordLength; index++){
     password += characters.charAt(
